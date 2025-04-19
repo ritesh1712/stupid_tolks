7eb82a9be257fc0796135d0ea04fe9691b09483d
ritesh1712 <105135021+ritesh1712@users.noreply.github.com>
2025-04-19 13:57:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className='overflow-hidden'>
+      <p className='text-red-700 text-7xl'>
+        ritesh
+      </p>
+   </div>
+  );
+}
+
+export default App;
